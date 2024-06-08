e220c3f096469a73f58b32c59f446b0a8ed7e1dc
jccl <jccl@cin.ufpe.br>
2024-06-07 21:54:54 -0300

    fix

--- backend/dist/main.js
+++
@@ -1,7 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-const server_1 = require("./aplication/web/server");
-function run() {
-    (0, server_1.initServerGraphql)();
-}
-run();
