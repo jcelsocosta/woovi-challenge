1199c7137618252121b7ccb74f603198c667e15d
jccl <jccl@cin.ufpe.br>
2024-06-13 16:18:19 -0300

    fix

--- backend/src/domain/usecase/transaction.ts
+++ backend/src/domain/usecase/transaction.ts
@@ -82,10 +82,12 @@ class TransactionUseCase {
             const idempotencyKey = crypto.createHash('sha256').update(combined).digest('hex')
 
             const transaction = await this.transactionRepository.findOne({ where: { idempotencyKey: idempotencyKey }, order: { createdDate: 'DESC' }})
+            const currentDate = dateNow()
             // console.log('p1', moment().format(transaction?.createdDate.toString()))
-            // console.log('p2', moment().subtract(3, 'minutes').format())
+            // console.log('p2', moment(currentDate).subtract(1, 'minutes').format())
+            
             // console.log('date', moment().format(transaction?.createdDate.toString()) >= moment().subtract(3, 'minutes').format())
-            if (transaction && (moment().format(transaction.createdDate.toString()) >= moment().add(1, 'minutes').format())) {
+            if (transaction && (moment().format(transaction.createdDate.toString()) >= moment(currentDate).subtract(1, 'minutes').format())) {
               const output: CreateTransactionOutput = {
                 transaction: transaction,
                 error: null
