2b0fd5860e7fa963457032f488eb0ba3084fd79c
jccl <jccl@cin.ufpe.br>
2024-06-13 15:44:38 -0300

    fix

--- bank-ui/src/components/Default.tsx
+++ bank-ui/src/components/Default.tsx
@@ -1,6 +1,19 @@
+import { useEffect } from 'react'
+import { useNavigate } from 'react-router-dom'
+
 export interface IDefaultProps extends React.ComponentPropsWithoutRef<'div'> {}
 
 export default function Default({}: IDefaultProps) {
+  const token = localStorage.getItem('token')
+  const navigate = useNavigate()
+
+  useEffect(() => {
+    if (token) {
+      navigate('/home')
+    } else {
+      navigate('login')
+    }
+  }, [])
   return (
     <>
       <div></div>
